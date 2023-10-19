9f7327cd03d1b3c15f68c5b9498d23b77d895b72
Pranavan Subendiran <subendiranp@gmail.com>
2023-10-19 11:24:27 +0530

    Pay Page

--- frontend/src/pages/Pay.jsx
+++ frontend/src/pages/Pay.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Pay = () => {
+
+    const handleClick = () =>{
+        document.getElementById('paid').innerHTML = "You have successfully purchased the Tickets!";
+    }
+
   return (
-    <div>Pay</div>
+    <div>
+        <p id = 'paid'></p>
+        <button onClick = {handleClick}>Pay</button>
+    </div>
+    
   )
 }
 
-export default Pay
+export default Pay;
